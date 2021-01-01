d4b861944a4eb1204079d762e0054b5fb73d45e1
Bruno Brito <brunobrito@MacBook-Air-de-Bruno.local>
2021-01-01 18:55:20 -0300

    start do projeto alura viagens

---
+++ Alura Viagens/Alura Viagens/Alura_ViagensApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alura_ViagensApp.swift
+//  Alura Viagens
+//
+//  Created by Bruno Brito on 01/01/21.
+//
+
+import SwiftUI
+
+@main
+struct Alura_ViagensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Alura Viagens/Alura Viagens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Alura Viagens
+//
+//  Created by Bruno Brito on 01/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
