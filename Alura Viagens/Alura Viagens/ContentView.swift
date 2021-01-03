0609f57ca1e2e8a7f7d3db9a2f3bb1a8e78e31d1
Bruno Brito <leaodebrito@icloud.com>
2021-01-03 11:32:41 -0300

    desenvolvimento da header (cabeçalho) da interface

--- Alura Viagens/Alura Viagens/ContentView.swift
+++ Alura Viagens/Alura Viagens/ContentView.swift
@@ -8,21 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let paddingHorizontal: CGFloat = 30
+    let paddingTopView: CGFloat = 50
+    
     var body: some View {
-        VStack{
-            Text("Alura Viagens")
-            Text("ESPACIAL")
-            Text("BRAZIL")
+        
+        
+        GeometryReader{ view in
             
-            List{
-                Text("Rio de Janeiro")
-                Text("Ceará")
-                Text("Atibaia")
-                Text("Rio de Janeiro")
+            //MARK: - VStack principal
+            VStack{
+                
+                //MARK: - VStack Header
+                VStack{
+                    Text("Alura Viagens")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .padding(.top, paddingTopView)
+                        
+                    Text("ESPECIAL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Book", size: 20))
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 20, alignment: .leading)
+                        .padding(.horizontal, paddingHorizontal)
+                    Text("BRASIL")
+                        .foregroundColor(Color.white)
+                        .font(.custom("Avenir Black", size: 20))
+                        .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 20, alignment: .leading)
+                        .padding(.horizontal, paddingHorizontal)
+                }
+                .frame(width: view.size.width, height: 180, alignment: .top)
+                .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.purple/*@END_MENU_TOKEN@*/)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                List{
+                    Text("Rio de Janeiro")
+                    Text("Ceará")
+                    Text("Atibaia")
+                    Text("Rio de Janeiro")
+                    
+                    
+                }
             }
         }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
